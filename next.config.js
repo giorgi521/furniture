6e15b2443c4fd34ecdc7568ef8e213eee7d486fa
giorgi gogoladze <gogoladzeg321@gmail.com>
2023-07-11 17:03:38 +0400

    fixed nextConfig file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['en-US', 'ka-GE'],
